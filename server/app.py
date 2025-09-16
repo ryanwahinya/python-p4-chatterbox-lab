4be0f8c56d7e11b095571465c501d2fe03afab0e
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-09-16 09:51:37 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
-from builtins import Exception, str
-from flask import Flask, request, jsonify
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -10,46 +9,90 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# manage cross origin issues
+# @cross_origin() for specific routes
 CORS(app)
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
-@app.route('/messages', methods=['GET'])
-def get_messages():
-    messages = Message.query.order_by(Message.created_at.asc()).all()
-    return jsonify([m.to_dict() for m in messages]), 200
 
-@app.route('/messages', methods=['POST'])
-def create_message():
-    data = request.get_json()
-    try:
+@app.route("/")
+def home():
+    return f"ChatBox"
+
+
+@app.route('/messages', methods=["GET", "POST"])
+def messages():
+
+    if request.method == "GET":
+        messages_lc = [message.to_dict() for message in Message.query.all()]
+
+        response = make_response(jsonify(messages_lc), 200)
+
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+    elif request.method == "POST":
+        # different types of input
+        # new_message = Message(body=request.form.get(
+        #     "body"), username=request.form.get("username"))
+
+        data = request.get_json()
+        
         new_message = Message(
-            username=data['username'],
-            body=data['body']
+            body=data["body"],
+            username=data["username"]
         )
+
         db.session.add(new_message)
         db.session.commit()
-        return jsonify(new_message.to_dict()), 201
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
 
-@app.route('/messages/<int:id>', methods=['PATCH'])
-def update_message(id):
-    message = Message.query.get_or_404(id)
-    data = request.get_json()
+        new_message_dict = new_message.to_dict()
 
-    if 'body' in data:
-        message.body = data['body']
+        response = make_response(jsonify(new_message_dict), 201)
 
-    db.session.commit()
-    return jsonify(message.to_dict()), 200
+        response.headers["Content-Type"] = "application/json"
 
-@app.route('/messages/<int:id>', methods=['DELETE'])
-def delete_message(id):
-    message = Message.query.get_or_404(id)
-    db.session.delete(message)
-    db.session.commit()
-    return '', 204
+        return response
+
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
+def messages_by_id(id):
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == "PATCH":
+        updated_message = request.get_json()
+
+        for attr in updated_message:
+            setattr(message, attr, updated_message.get(attr))
+
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(jsonify(message_dict), 200)
+
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "success": True,
+            "message": "Message deleted"
+        }
+
+        response = make_response(jsonify(response_body), 200)
+
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
 
 if __name__ == '__main__':
-    app.run(port=4000)
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
-from datetime import datetime
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,16 +8,12 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    body = db.Column(db.String, nullable=False)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    serialize_rules = ('-updated_at',)  # Exclude updated_at from serialization
-
-    def __repr__(self):
-        return f'<Message {self.id} by {self.username}>'
