b8374f7a3a366ec54fbf34757f103bf50ab8f17a
RyanWahinya <ryan.wahinya@student.moringaschool.com>
2025-09-16 09:44:40 +0300

    new commit

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, jsonify
+from builtins import Exception, str
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
@@ -11,36 +12,44 @@ app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/messages',methods =['GET','POST']) 
-def messages():
-    if request.method == 'GET':
-        messages = Message.query.order_by(Message.created_at.asc()).all()
-        message_dict = [message.to_dict() for message in messages]
-        return make_response(jsonify(message_dict), 200)
-    elif request.method == 'POST':
-        body = request.json['body']
-        username = request.json['username']
-        message = Message(body=body, username=username)
-        db.session.add(message)
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages]), 200
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    try:
+        new_message = Message(
+            username=data['username'],
+            body=data['body']
+        )
+        db.session.add(new_message)
         db.session.commit()
-        return jsonify(message.to_dict())
-@app.route('/messages/<int:id>',methods =['PATCH','DELETE'])
-def messages_by_id(id):
-    if request.method == 'PATCH':
-        message = Message.query.get(id)
-        message.body = request.json['body']
-        db.session.add(message)
-        db.session.commit()
-        return jsonify(message.to_dict())
-    
-    elif request.method == 'DELETE':
-        message = Message.query.get(id)
-        db.session.delete(message)
-        db.session.commit()
-        return jsonify(message.to_dict())
+        return jsonify(new_message.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+
+    if 'body' in data:
+        message.body = data['body']
+
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
